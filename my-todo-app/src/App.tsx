f82ed9283412eda4c3c25412bb6731cd0e8ada21
Kurakula Govardhan <46017384+kgovardhan409@users.noreply.github.com>
2025-04-06 20:51:47 +0530

    initial commit

---
+++ my-todo-app/src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">To Do App</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// html + javascript -- JSX
+// public folder - static assets,
+
+// Task
+// crate to do component(ToDoComponent) and add it to the app
+// In ToDoComponent create a form  - label(add new todos) input field and button(add)
+
+// next day
+// props - parent to child..
+// state in tod do component
